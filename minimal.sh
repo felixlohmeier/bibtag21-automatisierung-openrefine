106ecefb7b4aec19aa4d58cec8fe3ad6d7a928e0
Felix Lohmeier <mail@felixlohmeier.de>
2021-06-09 11:02:11 +0200

    Erläuterungen für Minimalversion des Scripts

--- minimal.sh
+++ minimal.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-openrefine/refine -d data &
-sleep 15
+rm -r openrefine/data # Arbeitsverzeichnis leeren
+openrefine/refine -d data & # OpenRefine als Hintergrundprozess starten
+sleep 15 # warten bis OpenRefine gestartet ist
 openrefine-client --create schriftstellerinnen.csv
 openrefine-client --apply history.json schriftstellerinnen
 openrefine-client --export schriftstellerinnen
-kill %1
-wait
-rm -r openrefine/data
+kill %1 # OpenRefine beenden
+wait # warten bis OpenRefine beendet ist
